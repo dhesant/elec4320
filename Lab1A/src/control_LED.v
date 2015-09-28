4708296066d1a9d2c44764790b0f9831b65a06c9
Dhesant Nakka <dhesant@gmail.com>
2015-09-28 22:24:07 +0800

    Added files from canvas

---
+++ Lab1A/src/control_LED.v
@@ -0,0 +1,12 @@
+// Lab 1A: ELEC 4440- Reconfigurable Computing: From Theory to Practice
+// Department of ECE, Hong Kong University of Science & Technology
+// Controlling the LEDs using DIP switches on 
+
+module control_LED
+( input [7:0] input_value,
+  output [7:0] show_value
+);
+
+//Insert your code to assign to output port
+
+endmodule
